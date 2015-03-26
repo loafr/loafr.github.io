286cc0c482c12469bced6e9b7c5a1bda114c46ad
lrobins <lrobins@adobe.com>
2015-03-25 19:46:13 -0600

    adds ngRoute and the 3 templates ill be using

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
-var app = angular.module('lrApp', []);
+var app = angular.module('lrApp', ['ngRoute']);
 
-//routes
+app.config(function($routeProvider, $locationProvider){
+
+  $routeProvider
+  .when('/', {
+  	templateUrl: 'js/homeTemplate.html',
+  	controller: 'MainCtrl'
+  })
+  .when('/about', {
+  	templateUrl: 'js/aboutTemplate.html',
+  	controller: 'MainCtrl'
+  })
+  .when('/projects', {
+  	templateUrl: 'js/projectsTemplate.html',
+  	controller: 'MainCtrl'
+  })
+  .otherwise ({
+  	redirectTo: '/'
+  })
+
+  $locationProvider.html5Mode(false);
+
+});
