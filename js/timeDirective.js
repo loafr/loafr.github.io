0b4bca4a7ca3b2d9f47f8a21623ecfa01571ebbd
lrobins <lrobins@adobe.com>
2015-03-26 19:18:23 -0600

    minor changes, no functionality

--- js/timeDirective.js
+++ js/timeDirective.js
@@ -14,3 +14,17 @@ angular.module('lrApp').directive('clock', function(dateFilter, $timeout) {
         }
     };
 });
+
+// var slideToggle = function() {
+//   console.log('i am defined');
+// };
+
+// angular.module('lrApp').directive('toggle', function() {
+//     return function($scope, $elem, $attrs) {
+//         $scope.$on('event:toggle', function(ev,num) {
+//             if(num===$attrs.toggle){
+//                 $elem.slideToggle();
+//             }
+//         });
+//     };
+// });
