d3c78f9ffe39e756f32f3d41e782872716e78a1b
Logan R <loafr@users.noreply.github.com>
2016-08-01 17:27:31 -0600

    v2 of adding target page

---
+++ loafr.github.io/js/app.js
@@ -0,0 +1,28 @@
+var app = angular.module('lrApp', ['ngRoute', 'ngAnimate']);
+
+app.config(function($routeProvider, $locationProvider){
+
+  $routeProvider
+  .when('/', {
+  	templateUrl: 'js/homeTemplate.html',
+  	controller: 'MainCtrl'
+  })
+  .when('/about', {
+  	templateUrl: 'js/aboutTemplate.html',
+  	controller: 'MainCtrl'
+  })
+  .when('/projects', {
+  	templateUrl: 'js/projectsTemplate.html',
+  	controller: 'MainCtrl'
+  })
+  .when('/target', {
+    templateUrl: 'js/targetExample.html',
+    controller: 'MainCtrl'
+  })
+  .otherwise ({
+  	redirectTo: '/'
+  })
+
+  $locationProvider.html5Mode(false);
+
+});
