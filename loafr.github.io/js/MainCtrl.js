0dcf22223dfb346146f8b8ce4e26c970c3bd6fc0
Logan R <loafr@users.noreply.github.com>
2016-08-02 14:00:59 -0600

    trying to add modal service

--- loafr.github.io/js/MainCtrl.js
+++ loafr.github.io/js/MainCtrl.js
@@ -1,3 +1,19 @@
-angular.module('lrApp').controller('MainCtrl', function($scope, mainService) {
-	//
+angular.module('lrApp').controller('MainCtrl', function($scope, mainService, ModalService) {
+	ModalService.showModal({
+	    templateUrl: "modal.html",
+	    controller: "ModalController"
+	  }).then(function(modal) {
+
+	    //it's a bootstrap element, use 'modal' to show it
+	    modal.element.modal();
+	    modal.close.then(function(result) {
+	      console.log(result);
+	    });
+	  });
+});
+
+app.controller('ModalController', function($scope, close) {
+
+  // when you need to close the modal, call close
+  close("Success!");
 });
